5c9e880853339cfac350e350a1e99e1b4b53df24
fftcysstkm <t.satobm3012@gmail.com>
2021-12-31 19:55:17 +0900

    画像ファイルを表示するよう修正

--- app/src/main/java/com/demo/android/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/demo/android/affirmations/adapter/ItemAdapter.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.demo.android.affirmations.R
 import com.demo.android.affirmations.model.Affirmation
 
+/*
+* 格言をRecyclerViewに適用させるアダプター
+*
+* */
 // 文字列リソースを取得するためにContexが必要（リソースなどアプリ内横断的な情報を保持）
 class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
@@ -16,6 +21,7 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
     // ビューホルダー：個々のリスト項目ビューへの参照を保持する
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     // RecyclerView の新しいビューホルダーを作成する
@@ -29,8 +35,10 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
 
     // リスト項目ビューの内容を置き換えるのに用いられるメソッド（レイアウトマネージャがこのメソッドを用いる）
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        // AffirmationはStringと画像のリソースIDを保持
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     // データセットのサイズを返す(この書き方は省略記法)
--- app/src/main/java/com/demo/android/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/demo/android/affirmations/model/Affirmation.kt
@@ -1,8 +1,15 @@
 package com.demo.android.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 /*
-* 「格言」へのリソースIDを保持するDataクラス
+* 「格言」のStringリソースおよび画像リソースIDを保持するDataクラス
+* (使用者がIDを間違えないようにアノテーション付与)
 * */
-data class Affirmation(val stringResourceId: Int) {
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+) {
 
 }
